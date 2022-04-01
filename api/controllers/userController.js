9e6b5dabc5033bd70648989e1668cc250519bc89
stenzr <rkrohitkr01@gmail.com>
2022-04-01 19:41:35 +0530

    [ADD] signup logic

---
+++ api/controllers/userController.js
@@ -0,0 +1,32 @@
+// require essential modules
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// TODO: generate salt
+const encryptPassword = async (password) => {
+    return await bcrypt.hash(password, 10);
+}
+
+const validateFunction = async (password, passwordHash) => {
+    return await bcrypt.compare(password, passwordHash);
+}
+
+exports.signup = async (req, res, next) => {
+    try {
+        const { email, password, role } = req.body
+        const passwordHash = await encryptPassword(password);
+        const newUser = new User({ email, password: passwordHash, role: role || "normalUser" });
+        const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+            expiresIn: "Id"
+        });
+        newUser.accessToken = accessToken;
+        await newUser.save();
+        res.json({ 
+            data: newUser,
+            accessToken
+        })
+    } catch (error) {
+        next(error)
+    }
+}
