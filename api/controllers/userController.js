255a8544e8ff6b27ea0a62d1e6366855e4a6762b
stenzr <rkrohitkr01@gmail.com>
2022-04-01 22:54:16 +0530

    [ADD] route logic (get user)

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -26,7 +26,7 @@ exports.signup = async (req, res, next) => {
         });
         newUser.accessToken = accessToken;
         await newUser.save();
-        res.json({ 
+        res.json({
             data: newUser,
             accessToken
         })
@@ -55,4 +55,33 @@ exports.login = async (req, res, next) => {
         next(error);
     }
 }
+
+// ------------------- route logic -------------------------
+
+// ------------------- get all users -----------------------
+
+exports.getUsers = async (req, res, next) => {
+    const users = await User.find({});
+    res.status(200).json({
+        data: users
+    });
+}
+
+
+// -------------------- get user by specific id ---------------
+
+exports.getUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const user = await User.findById(userId);
+        if (!user) return next(new Error("User not found"));
+        res.status(200).json({
+            data: user
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+// ------------------- update user details ---------------------
 
